1722a8f39bb3e5e36abc4cbcca812724380bc82c
Nicholas Schweitzer <f3bl@gmx.com>
2020-12-27 09:04:24 +0100

    Debug messenger works properly

--- testing.cpp
+++ testing.cpp
@@ -12,9 +12,9 @@ const uint32_t INIT_WIDTH = 800, INIT_HEIGHT = 600;
 const std::vector<const char*> VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
 
 #ifdef NDEBUG
-const bool VALIDATION_LAYERS_ON = true;
-#else
 const bool VALIDATION_LAYERS_ON = false;
+#else
+const bool VALIDATION_LAYERS_ON = true;
 #endif
 
 struct GWindow {
@@ -22,8 +22,7 @@ struct GWindow {
 
 	// Pointers are valid until GLFW terminates (so until the GWindow is
 	// destroyed)
-	uint32_t req_instance_ext_ct;
-	const char** req_instance_exts;
+	std::vector<const char*> req_instance_exts;
 
 	GWindow(uint32_t width, uint32_t height) {
 		glfwInit();
@@ -31,7 +30,10 @@ struct GWindow {
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
 
-		req_instance_exts = glfwGetRequiredInstanceExtensions(&req_instance_ext_ct);
+		const char** raw_extensions;
+		uint32_t extension_ct;
+		raw_extensions = glfwGetRequiredInstanceExtensions(&extension_ct);
+		req_instance_exts = {raw_extensions, raw_extensions + extension_ct};
 	}
 
 	~GWindow() {
@@ -69,10 +71,29 @@ bool check_instance_ext_support(std::vector<const char*> req_exts) {
 	});
 }
 
+static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT severity,
+						     VkDebugUtilsMessageTypeFlagsEXT,
+						     const VkDebugUtilsMessengerCallbackDataEXT* data,
+						     void*) {
+	if (severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+		std::cerr << "validation layer: " << data->pMessage << std::endl;
+
+	return VK_FALSE;
+}
+
+VkResult create_debug_msgr(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* info,
+			   VkDebugUtilsMessengerEXT* msgr) {
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func) return func(instance, info, nullptr, msgr);
+	else return VK_ERROR_EXTENSION_NOT_PRESENT;
+}
+
 int main() {
 	auto glfw_window = GWindow(INIT_WIDTH, INIT_HEIGHT);
 
 	// Create instance
+	VkInstance instance;
+
 	VkApplicationInfo app_info{};
 	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	app_info.pApplicationName = "My Thingy";
@@ -81,12 +102,20 @@ int main() {
 	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	app_info.apiVersion = VK_API_VERSION_1_0;
 
-	VkInstance instance;
 	VkInstanceCreateInfo instance_info{};
+
+	VkDebugUtilsMessengerCreateInfoEXT debug_msgr_info{};
+	debug_msgr_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	debug_msgr_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	debug_msgr_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	debug_msgr_info.pfnUserCallback = debug_callback;
+
+	if (VALIDATION_LAYERS_ON) instance_info.pNext = &debug_msgr_info;
+
 	instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	instance_info.pApplicationInfo = &app_info;
 
-	std::vector<const char*> extensions(glfw_window.req_instance_exts, glfw_window.req_instance_exts + glfw_window.req_instance_ext_ct);
+	auto extensions = glfw_window.req_instance_exts;
 	if (VALIDATION_LAYERS_ON) extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 	if (!check_instance_ext_support(extensions))
 		throw std::runtime_error("Not all required instance extensions supported!");
@@ -105,7 +134,16 @@ int main() {
 	if(vkCreateInstance(&instance_info, nullptr, &instance) != VK_SUCCESS)
 		throw std::runtime_error("Failed to create instance!");
 
+	if (VALIDATION_LAYERS_ON) {
+		// Create debug messenger
+		VkDebugUtilsMessengerEXT debug_msgr;
+		if (create_debug_msgr(instance, &debug_msgr_info, &debug_msgr) != VK_SUCCESS)
+			throw std::runtime_error("Failed to create debug messenger!");
+	}
+
 	while (!glfwWindowShouldClose(glfw_window.window)) {
 		glfwPollEvents();
 	}
+
+	vkDestroyInstance(instance, nullptr);
 }
