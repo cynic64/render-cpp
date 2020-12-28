41ceaea38719e353e135f57779bd8d4f7d6f80f2
Nicholas Schweitzer <f3bl@gmx.com>
2020-12-28 09:11:07 +0100

    Create device

--- testing.cpp
+++ testing.cpp
@@ -1,10 +1,12 @@
 #include "vk_instance.hpp"
+#include <vulkan/vulkan_core.h>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <optional>
 
 const uint32_t INIT_WIDTH = 800, INIT_HEIGHT = 600;
 
@@ -47,9 +49,59 @@ int main() {
 	VkDebugUtilsMessengerEXT debug_msgr;
 	vk_instance::create(glfw_window.req_instance_exts, {}, VALIDATION_ENABLED, &instance, &debug_msgr);
 
+	// Create physical device
+	uint32_t phys_dev_ct;
+	vkEnumeratePhysicalDevices(instance, &phys_dev_ct, nullptr);
+	if (phys_dev_ct == 0) throw std::runtime_error("No GPUs found!");
+
+	std::vector<VkPhysicalDevice> phys_devs(phys_dev_ct);
+	vkEnumeratePhysicalDevices(instance, &phys_dev_ct, phys_devs.data());
+	auto phys_dev = phys_devs[0];
+	VkPhysicalDeviceProperties phys_dev_props;
+	vkGetPhysicalDeviceProperties(phys_dev, &phys_dev_props);
+	std::cout << "Using device: " << phys_dev_props.deviceName << std::endl;
+
+	// Find queue family
+	std::optional<uint32_t> graphics_fam;
+	uint32_t queue_fam_ct;
+	vkGetPhysicalDeviceQueueFamilyProperties(phys_dev, &queue_fam_ct, nullptr);
+	std::vector<VkQueueFamilyProperties> queue_fams(queue_fam_ct);
+	vkGetPhysicalDeviceQueueFamilyProperties(phys_dev, &queue_fam_ct, queue_fams.data());
+
+	for (size_t i = 0; i < queue_fam_ct; ++i) {
+		if (queue_fams[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			graphics_fam = i;
+			break;
+		}
+	}
+
+	if (!graphics_fam.has_value()) throw std::runtime_error("Could not find queue family with graphics support!");
+
+	// Create logical device
+	VkDevice device;
+
+	VkDeviceQueueCreateInfo dev_queue_info{};
+	dev_queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	dev_queue_info.queueCount = 1;
+	dev_queue_info.queueFamilyIndex = graphics_fam.value();
+	float queue_priority = 1.0f;
+	dev_queue_info.pQueuePriorities = &queue_priority;
+
+	VkPhysicalDeviceFeatures dev_features{};
+
+	VkDeviceCreateInfo device_info{};
+	device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_info.queueCreateInfoCount = 1;
+	device_info.pQueueCreateInfos = &dev_queue_info;
+	device_info.pEnabledFeatures = &dev_features;
+
+	if (vkCreateDevice(phys_dev, &device_info, nullptr, &device) != VK_SUCCESS) throw std::runtime_error("Could not create device!");
+
 	while (!glfwWindowShouldClose(glfw_window.window)) {
 		glfwPollEvents();
 	}
+
+	vkDestroyDevice(device, nullptr);
 
 	vk_instance::destroy_debug_msgr(instance, debug_msgr);
 	vkDestroyInstance(instance, nullptr);
